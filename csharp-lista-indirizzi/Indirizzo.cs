f0709c45ca1bc2a6c71b08900ad52334b26a1c73
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-29 16:45:17 +0200

    Add Indirizzo class

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZIP { get; set; }
+        
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZIP = zip;
+        }
+
+        public override string ToString() => $"NAME: {Name}, SURNAME: {Surname}, STREET: {Street}, CITY: {City}, PROVINCE: {Province}, ZIP: {ZIP}";
+    }
+}
